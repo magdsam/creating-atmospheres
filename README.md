758516eb8e1d48b00a07eea2ab0f8e2d911a9625
magdsam <samoniggma@gmail.com>
2022-12-13 19:37:22 +0100

    :tada: first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Atmospheres Wordpress Theme
+
+More Info for developing the Theme can be found in the [Theme Folder](/wp-content/themes/atmospheres/README.md)
---
+++ wp-content/mu-plugins/acorn-load.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: Acorn Autoloader
+Plugin URI: http://studiobrighten.com/
+Description: Custom Autoloader for Roots Acorn Plugin required by Sage
+Author: Studio Brighten
+Version: 1.0
+Author URI: http://studiobrighten.com/
+*/
+
+require WPMU_PLUGIN_DIR . '/acorn/acorn.php';
