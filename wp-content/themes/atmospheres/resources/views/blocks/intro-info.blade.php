78789c5f8f6d8b83e623c3a8f26eb3d26a0302d5
magdsam <samoniggma@gmail.com>
2022-12-15 23:11:36 +0100

    :sparkles: added destop landingpage blocks

--- wp-content/themes/atmospheres/resources/views/blocks/intro-info.blade.php
+++ wp-content/themes/atmospheres/resources/views/blocks/intro-info.blade.php
@@ -1,28 +1,55 @@
 <div class="{{ $block->classes }} relative">
   @if($video)
-  <div class="wp-block-intro-info__video w-full h-full overflow-hidden rounded-t-full">
-    <video width="{{ $video['width'] }}" height="{{ $video['height'] }}" class="w-full h-auto blur-md scale-105" autoplay muted loop playsinline>
+  <div class="wp-block-intro-info__video w-full h-full overflow-hidden rounded-t-full absolute left-0 top-0 z-[-1]">
+    <video width="{{ $video['width'] }}" height="{{ $video['height'] }}" class="w-full h-auto blur-xl scale-105" autoplay muted loop playsinline>
       <source src="{{ $video['url'] }}" type="{{$video['mime_type']}}">
     </video>
   </div>
   @endif
-  @if ($taglines_1)
-  <div class="wp-block-intro-info__taglines wp-block-intro-info__taglines--1">
-    @foreach ($taglines_1 as $item)
-    <div class="wp-block-intro-info__tagline">{!! $item['tagline'] !!}</div>
-    @endforeach
-  </div>
-  @endif
-  @if ($taglines_2)
-    <div class="wp-block-intro-info__taglines wp-block-intro-info__taglines--2">
-      @foreach ($taglines_2 as $item)
-        <div class="wp-block-intro-info__tagline">{!! $item['tagline'] !!}</div>
+  <div class="wp-block-intro-info__content">
+    @if ($taglines_1)
+    <div class="wp-block-intro-info__taglines wp-block-intro-info__taglines--1">
+      @foreach ($taglines_1 as $item)
+      <div class="wp-block-intro-info__tagline">{!! $item['tagline'] !!}</div>
       @endforeach
     </div>
-  @endif
-  @if ($countdown_info)
-  <div class="wp-block-intro-info__tagline">
-    {!! $countdown_info !!}
+    @endif
+    @if ($taglines_2)
+    <div class="wp-block-intro-info__taglines wp-block-intro-info__taglines--2">
+      @foreach ($taglines_2 as $item)
+      <div class="wp-block-intro-info__tagline">{!! $item['tagline'] !!}</div>
+      @endforeach
+    </div>
+    @endif
+    @if ($countdown_info)
+    <div class="wp-block-intro-info__taglines wp-block-intro-info__taglines--countdown">
+      <div class="wp-block-intro-info__tagline">
+        {!! $countdown_info !!}
+      </div>
+      <div class="wp-block-intro-info__countdown">
+        <div class="wp-block-intro-info__countdown__content flex">
+          <div class="wp-block-intro-info__countdown__item days">
+            <div class="number">00</div>
+            <div class="legend">Days</div>
+          </div>
+          <div class="wp-block-intro-info__countdown__divider">:</div>
+          <div class="wp-block-intro-info__countdown__item hours">
+            <div class="number">00</div>
+            <div class="legend">Hours</div>
+          </div>
+          <div class="wp-block-intro-info__countdown__divider">:</div>
+          <div class="wp-block-intro-info__countdown__item minutes">
+            <div class="number">00</div>
+            <div class="legend">Minutes</div>
+          </div>
+          <div class="wp-block-intro-info__countdown__divider">:</div>
+          <div class="wp-block-intro-info__countdown__item seconds">
+            <div class="number">00</div>
+            <div class="legend">Seconds</div>
+          </div>
+        </div>
+      </div>
+    </div>
+    @endif
   </div>
-  @endif
 </div>
--- wp-content/themes/atmospheres/resources/views/sections/footer.blade.php
+++ wp-content/themes/atmospheres/resources/views/sections/footer.blade.php
@@ -1,3 +1,23 @@
-<footer class="content-info">
-  @php(dynamic_sidebar('sidebar-footer'))
+<footer class="content-info mb-12 absolute bottom-0 left-0 w-full">
+<div class="content-info__inner flex justify-between px-8 2xl:px-16 pt-9">
+    <div class="content-info__logos flex gap-x-[6.25rem] items-center">
+      <div class="content-info__logos__logo">
+        @svg('images.idk-logo')
+      </div>
+      <div class="content-info__logos__logo">
+        @svg('images.fh-joanneum-logo')
+      </div>
+      <div class="content-info__logos__logo">
+        @svg('images.esc-logo')
+      </div>
+      <div class="content-info__logos__logo text-xl">
+        @svg('images.kunstuni-logo')
+      </div>
+    </div>
+    <div class="content-info__cta">
+      @hasoption('header-cta')
+      @option('header-cta')
+      @endoption
+    </div>
+  </div>
 </footer>
