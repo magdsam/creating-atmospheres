7b4400e44c907873cbc2d0f78c13adb4b167857f
magdsam <samoniggma@gmail.com>
2022-12-16 16:35:32 +0100

    :heavy_plus_sign: updated dependencies

--- wp-content/themes/atmospheres/bud.config.mjs
+++ wp-content/themes/atmospheres/bud.config.mjs
@@ -29,7 +29,7 @@ export default async (app) => {
     /**
      * Proxy origin (`WP_HOME`)
      */
-    .proxy('http://creatingatmospheres.local')
+    .proxy('https://creatingatmospheres.local')
 
     /**
      * Development origin
