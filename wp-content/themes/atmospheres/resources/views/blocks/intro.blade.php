552eb88a61ba2256e07bd691e2170a0042640aa7
magdsam <samoniggma@gmail.com>
2022-12-18 15:11:22 +0100

    :lipstick: mobile styling

--- wp-content/themes/atmospheres/resources/views/blocks/intro.blade.php
+++ wp-content/themes/atmospheres/resources/views/blocks/intro.blade.php
@@ -1,17 +1,17 @@
-<div class="{{ $block->classes }} pt-60 pb-20 px-8 2xl:px-16">
-  <div class="wp-block-intro__inner flex justify-between items-center gap-x-5">
-    <div class="wp-block-intro__info wp-block-intro__info--left w-1/4 text-xl font-medium">
+<div class="{{ $block->classes }} pt-60 pb-20 px-4 md:px-8 2xl:px-16">
+  <div class="wp-block-intro__inner flex flex-col gap-y-6 lg:flex-row justify-between items-center gap-x-5">
+    <div class="wp-block-intro__info wp-block-intro__info--left lg:w-1/4 text-center lg:text-left text-base md:text-xl font-medium">
       {!! $info_left !!}
     </div>
-    <div class="wp-block-intro__logo">
-      @svg('images.logo-centered')
+    <div class="wp-block-intro__logo order-first lg:order-none">
+      @svg('images.logo-centered', ['class' => 'w-full lg:w-[51rem]'])
     </div>
-    <div class="wp-block-intro__info wp-block-intro__info--right w-1/4 text-right text-xl font-medium">
+    <div class="wp-block-intro__info wp-block-intro__info--right lg:w-1/4 text-center lg:text-right text-base md:text-xl font-medium">
       {!! $info_right !!}
     </div>
   </div>
   @if ($subtitle)
-    <div class="wp-block-intro__subtitle text-center uppercase text-5xl mt-6 font-light">
+    <div class="wp-block-intro__subtitle text-center uppercase text-2xl lg:text-5xl mt-6 font-light">
       {{ $subtitle }}
     </div>
   @endif
--- wp-content/themes/atmospheres/resources/views/sections/header.blade.php
+++ wp-content/themes/atmospheres/resources/views/sections/header.blade.php
@@ -1,6 +1,6 @@
 <header class="banner fixed top-0 left-0 w-full">
-  <div class="banner__inner flex justify-between px-8 2xl:px-16 pt-9">
-    <div class="banner__logos flex gap-x-[6.25rem] items-center">
+  <div class="banner__inner flex flex-col md:flex-row justify-between px-8 2xl:px-16 pt-9">
+    <div class="banner__logos flex flex-wrap gap-x-4 lg:gap-x-[6.25rem] items-center">
       <div class="banner__logos__logo">
         @svg('images.idk-logo')
       </div>
